81ee0a4e233cb39886af0fe665127eda6897159b
evnwttn <evnwttn@gmail.com>
2022-08-17 19:09:52 -0230

    Update BFB.js

--- my-app/src/components/BFB.js
+++ my-app/src/components/BFB.js
@@ -1,12 +1,21 @@
 import "../style.css";
 
+const axios = require("axios").default;
+
 export const BFB = () => {
-  function sayHello() {
-    alert("You clicked me!");
+  function pushDeButton() {
+    axios
+      .post("http://localhost:5000/contact", {
+        data: "hello",
+      })
+      .then((_data) => console.log(JSON.stringify(_data)))
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   return (
-    <button id="bfb" onClick={sayHello}>
+    <button id="bfb" onClick={pushDeButton}>
       BFB
     </button>
   );
