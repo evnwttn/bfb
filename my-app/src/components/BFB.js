058b4a3c32754abcfd9fd6d39ad480415110f2c9
evnwttn <evnwttn@gmail.com>
2022-09-05 11:28:39 -0230

    it's a bfb

--- my-app/src/components/BFB.js
+++ my-app/src/components/BFB.js
@@ -1,17 +1,8 @@
 import "../style.css";
 
-const axios = require("axios").default;
-
 export const BFB = () => {
   function pushDeButton() {
-    axios
-      .post("http://localhost:5000/contact", {
-        data: "hello",
-      })
-      .then((_data) => console.log(JSON.stringify(_data)))
-      .catch(function (error) {
-        console.log(error);
-      });
+    console.log("hello");
   }
 
   return (
