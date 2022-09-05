6b9a742a7966cfc5648ea3a964690e47b0db97d2
evnwttn <evnwttn@gmail.com>
2022-09-05 11:30:13 -0230

    Update BFB.js

--- my-app/src/components/BFB.js
+++ my-app/src/components/BFB.js
@@ -1,8 +1,32 @@
 import "../style.css";
 
+const session = {
+  parameters: [
+    "title",
+    "Vocals",
+    "Percussion",
+    "Rhythm Guitar",
+    "Lead Guitar",
+    "Drums",
+    "Piano",
+    "FX",
+    "Mix",
+    "Master",
+  ],
+  author: "Button",
+  id: "Butt Sesh",
+  tracks: [
+    { title: "Track 1" },
+    { title: "Track 2" },
+    { title: "Track 3" },
+    { title: "Track 4" },
+    { title: "Track 5" },
+  ],
+};
+
 export const BFB = () => {
   function pushDeButton() {
-    console.log("hello");
+    console.log(session);
   }
 
   return (
